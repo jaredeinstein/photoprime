80ce779c365004950fad1a38ed2a0b968e7c1b9d
jaredeinstein <jared.einstein@gmail.com>
2015-10-01 19:54:24 +0000

    started to work on polaroid style

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,7 @@
 class PagesController < ApplicationController
     
     def index
-        #@photos = { color1: "green", color2: "blue" }
         @photos = Dir.glob("app/assets/images/jumbotron/*.jpg")
-        
-        respond_to do |format|
-            format.html
-            format.css
-        end
     end
     
 end
